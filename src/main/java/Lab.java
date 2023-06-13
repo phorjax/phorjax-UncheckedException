92c9255c5ae647dae85886668d129331969fa384
phorjax <joseph.12082@gmail.com>
2023-06-13 18:31:05 +0000

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,19 +1,28 @@
+import java.util.ArrayList;
 
 public class Lab {
     /**
      * This method must throw an unchecked exception.
-     * unchecked means that you do not need to wrap the method in a try/catch or a throws declaration.
-     * Unchecked exceptions may include situations like arithmetic errors like dividing by zero,
-     * accessing an index of an array which is out of bounds, stack overflow (result of infinite recursion), etc.
+     * unchecked means that you do not need to wrap the method in a try/catch or a
+     * throws declaration.
+     * Unchecked exceptions may include situations like arithmetic errors like
+     * dividing by zero,
+     * accessing an index of an array which is out of bounds, stack overflow (result
+     * of infinite recursion), etc.
      *
-     * For this challenge, I recommend creatively causing such an exception rather than using the 'throws' keyword,
-     * which would also work since RuntimeExceptions are still technically Exceptions.
+     * For this challenge, I recommend creatively causing such an exception rather
+     * than using the 'throws' keyword,
+     * which would also work since RuntimeExceptions are still technically
+     * Exceptions.
      *
-     * You could view the entire Exception family here: https://programming.guide/java/list-of-java-exceptions.html
-     * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
+     * You could view the entire Exception family here:
+     * https://programming.guide/java/list-of-java-exceptions.html
+     * Notice that errors are separate from exceptions. Errors are external to Java
+     * and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
-
+    public void throwUncheckedException() {
+            ArrayList<String> list = new ArrayList<>();
+            System.out.println(list.get(0));
     }
 }
